ae32108f3cadb911a226260e920dc0b57658d1cb
francescomarins <francesco.marinelli@outlook.com>
2023-01-10 11:34:30 +0100

    Challenge 2.2.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,6 @@ class App extends React.Component {
   fetchData() {
     let url_get = url + '?id_gte=' + Number(this.state.loaded+1) + '&id_lte=' + Number(this.state.loaded+bundle_size);
     fetch(url_get).then(resp => resp.json()).then(resp => {
-      console.log(resp);
       if(resp.length < bundle_size)
       document.getElementById('load-more').style.display = 'none';
       if(resp.length && this.state.posts.indexOf(resp[0].id)===-1) {
@@ -47,38 +46,8 @@ class App extends React.Component {
       this.setState({posts: updatingArray, loaded: updatingArray.length});
     }
 
-    handleUpdate(event) {
-      event.preventDefault();
-      let id = event.target.id.value.trim();
-
-      // Parameters' checks
-      if (!id || (!event.target.title.value.trim().length &&
-      !event.target.body.value.trim().length && !event.target.userId.value.trim().length)) {
-        alert("Fill all the fields properly");
-        return;
-      }
-      if (isNaN(id)) {
-        alert("The id must be a number");
-        return;
-      }
-
+    handleUpdate(id, title, body, userId) {
       let index = this.state.posts.findIndex(item => item.id === Number(id));
-      if(index === -1) {
-        alert("The post you want to update is not present");
-        return;
-      }
-
-      // Setting up variables for the request to the server
-      let item = this.state.posts[index];
-      let userId = (!event.target.userId.value.trim().length) ? item.userId : event.target.userId.value;
-      let title = (!event.target.title.value.trim().length) ? item.title : event.target.title.value;
-      let body = (!event.target.body.value.trim().length) ? item.body : event.target.body.value;
-
-      //Last check, done here to avoid doing it on an empty field which was not intended to be modified by the user
-      if (isNaN(userId)) {
-        alert("The userId must be a number");
-        return;
-      }
 
       //Request to server
       fetch(url+'/'+id, {
@@ -97,7 +66,6 @@ class App extends React.Component {
         console.log('Fetch error -> ' + err);
         return;
       });
-      document.getElementById("update-post-form").reset();
 
       //Updating the state with the edited list of posts
       let newItem = {};
@@ -108,6 +76,9 @@ class App extends React.Component {
       let updatingArray = this.state.posts.slice();
       updatingArray.splice(index, 1, newItem);
       this.setState({posts: updatingArray});
+
+      document.getElementById("post-"+id+"-content").style.display = 'block';
+      document.getElementById("update-post-"+id+"-form").style.display = 'none'
     }
 
     render() {
@@ -119,22 +90,9 @@ class App extends React.Component {
         <summary className='create-summary'>Add a new post</summary>
         <CreatePost/>
         </details>
-        <details>
-        <summary className='update-summary'>Modify an existing post</summary>
-        <form id="update-post-form" className="post-form" onSubmit={this.handleUpdate}>
-        Insert only data to be modified, a field left empty will be kept untouched.<br/>
-        Do not forget to insert the "ID" associated to the post to be updated.<br/>
-        Be aware that it is not possible to modify a post not shown below<br/>
-        <input type="text" id="id" name="id" placeholder='ID of the post to be updated'/><br/>
-        <input type="text" id="userId" name="userId" placeholder='UserId'/><br/>
-        <input type="text" id="title" name="title" placeholder='Title'/><br/>
-        <textarea id="body" name="body" placeholder="Body of the post"/><br/>
-        <input type="submit" value="Update"/>
-        </form>
-        </details>
         <h2>List of posts</h2>
         {this.state.posts.map((item) => (
-          <Post key={item.id} post={item} delete={this.removePostFromState}/>
+          <Post key={item.id} post={item} delete={this.removePostFromState} edit={this.handleUpdate}/>
         ))}
         <input type='button' id='load-more' value='Load Posts' onClick={this.fetchData}/>
         </div>
--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -5,11 +5,44 @@ class Post extends React.Component {
     super(props);
     this.state = { comments: [] };
     this.delete = this.delete.bind(this);
+    this.edit = this.edit.bind(this);
+    this.sendEdit = this.sendEdit.bind(this);
+    this.cancelEdit = this.cancelEdit.bind(this);
     this.loadComments = this.loadComments.bind(this);
   }
 
   delete() {
     this.props.delete(this.props.post.id)
+  }
+
+  edit() {
+    document.getElementById("post-"+this.props.post.id+"-content").style.display = 'none';
+    document.getElementById("update-post-"+this.props.post.id+"-form").style.display = 'block';
+  }
+
+  sendEdit(event) {
+    event.preventDefault();
+    let id = this.props.post.id;
+
+    // Setting up variables for the request to the server
+    let userId = (!event.target.userId.value.trim().length) ? this.props.post.userId : event.target.userId.value;
+    let title = (!event.target.title.value.trim().length) ? this.props.post.title : event.target.title.value;
+    let body = (!event.target.body.value.trim().length) ? this.props.post.body : event.target.body.value;
+
+    //Last check, done here to avoid doing it on an empty field which was not intended to be modified by the user
+    if (isNaN(userId)) {
+      alert("The userId must be a number");
+      return;
+    }
+
+    this.props.edit(id, title, body, userId);
+    document.getElementById("update-post-"+id+"-form").reset();
+  }
+
+  cancelEdit() {
+    document.getElementById("post-"+this.props.post.id+"-content").style.display = 'block';
+    document.getElementById("update-post-"+this.props.post.id+"-form").style.display = 'none';
+    document.getElementById("update-post-"+this.props.post.id+"-form").reset();
   }
 
   loadComments() {
@@ -18,7 +51,7 @@ class Post extends React.Component {
     .then(resp => {
       this.setState({comments: resp})
     });
-    document.getElementById("post-"+this.props.post.id).style.cursor = 'default';
+    document.getElementById("post-"+this.props.post.id+"-load-comments").style.display = 'none';
   }
 
   render() {
@@ -29,18 +62,29 @@ class Post extends React.Component {
     let body = post.body;
     return (
       <div id={"post-"+id} className="post">
-        <div className="post-content" onClick={this.loadComments}>
+      <form id={"update-post-"+id+"-form"} className="edit-form post-form" onSubmit={this.sendEdit}>
+      <input className="cancel-button" type="button" value="Cancel" onClick={this.cancelEdit}/><br/>
+      Please, leave as they are (empty) those fields that you want to keep unchanged.<br/>
+      <label>ID: <input type="text" id="id" name="id" placeholder={id} disabled/></label><br/>
+      <label>UserId: <input type="text" id="userId" name="userId" placeholder={user}/></label><br/>
+      <label>Title: <input type="text" id="title" name="title" placeholder={title}/></label><br/>
+      <textarea id="body" name="body" placeholder={body}/><br/>
+      <input type="submit" value="Update"/>
+      </form>
+        <div id={"post-"+id+"-content"} className="post-content">
           <div className="post-header">
-            <span className="user">User: {user}</span>
+            <span id={"post-"+id+"-user"} className="user">User: {user}</span>
             <span className='post-ops'>
               <span>ID: {id}</span>
               <input type='button' className="delete-button" value='Delete' onClick={this.delete}/>
+              <input type='button' className="edit-button" value='Edit' onClick={this.edit}/>
             </span>
           </div>
         <div>
-          <strong>{title}</strong>
+          <strong id={"post-"+id+"-title"} >{title}</strong>
         </div>
-          <div>{body}</div>
+          <div id={"post-"+id+"-body"} >{body}</div>
+          <input id={"post-"+id+"-load-comments"} type="button" className="load-comments" value="Show comments" onClick={this.loadComments}/>
           {this.state.comments.map((item) => (
             <div key={item.id} className='comment'>
               <div className='user'>Name: {item.name}</div>
